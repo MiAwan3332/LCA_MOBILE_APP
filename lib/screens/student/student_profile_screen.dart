992197116a38e8824d5bbd7b2e10ae4ecdcfce1e
Muhammad Adil <hur17728@gmail.com>
2024-07-22 15:49:22 +0500

    Changes to Student Profile

--- lib/screens/student/student_profile_screen.dart
+++ lib/screens/student/student_profile_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:lca_app/models/student_detail_model.dart';
+import 'package:lca_app/services/student_services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class StudentProfileScreen extends StatefulWidget {
   @override
-  _StudentProfileScreenState createState() => _StudentProfileScreenState();
+  _StudentProfileScreenState createState() =>
+      _StudentProfileScreenState();
 }
 
 class _StudentProfileScreenState extends State<StudentProfileScreen> {
-  String? name;
-  String? role;
+  Future<StudentDetail?>? futureStudentDetail;
 
   @override
   void initState() {
@@ -18,75 +20,70 @@ class _StudentProfileScreenState extends State<StudentProfileScreen> {
 
   Future<void> getdatafromsharedpreference() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      name = prefs.getString('name');
-      role = prefs.getString("role");
-    });
+    String? studentId = prefs.getString('studentId');
+    if (studentId != null) {
+      setState(() {
+        futureStudentDetail = Student().fetchStudentDetails(studentId, context);
+      });
+    } else {
+      setState(() {
+        futureStudentDetail =
+            Future.error('No student ID found in shared preferences');
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Fee Detail',
-          style: TextStyle(color: Colors.white, fontSize: 22),
-        ),
         backgroundColor: Color(0xFFBA8E4F),
-        iconTheme: IconThemeData(color: Colors.white),// Dark brown background
-        elevation: 0, // Remove elevation
-        
+        title:
+            Text('Profile', style: TextStyle(color: Colors.white)),
+        iconTheme: IconThemeData(color: Colors.white),
       ),
-       // Light beige background
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildProfileRow('Name:', name),
-            SizedBox(height: 16),
-            _buildProfileRow('Role:', role),
-            
-          ],
-        ),
-      ),
-    );
-  }
+      body: FutureBuilder<StudentDetail?>(
+        future: futureStudentDetail,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData) {
+            return Center(child: Text('No data available'));
+          } else {
+            StudentDetail student = snapshot.data!;
 
-  Widget _buildProfileRow(String label, String? value) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      decoration: BoxDecoration(
-        color: Color(0xFF6D4C41),
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            
-            color: Colors.grey.withOpacity(0.3),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Text(
-            label,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          SizedBox(width: 8),
-          Expanded(
-            child: value != null
-                ? Text(
-                    value,
-                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-                  )
-                : Center(
-                    child: CircularProgressIndicator(),
-                  ),
-          ),
-        ],
+            return Padding(
+              padding: EdgeInsets.all(20.0),
+              child: ListTile(
+                title: Text(
+                  'Name: ${student.name}',
+                  style: TextStyle(fontSize: 18),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 10),
+                    Text(
+                      'Email: ${student.email}',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Phone: ${student.phone}',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      'Admission Date: ${student.admission_date}',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
